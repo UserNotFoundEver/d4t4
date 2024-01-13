4d0cf4468a089726dcca1d45e3c7127cadce2c48
Sergio <sergiobluez@gmail.com>
2024-01-13 09:56:58 -0800

    src react

---
+++ App.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import './style.css'; // css file
+
+function D4T4Page() {
+    return (
+        <div>
+            <div className="navbar">
+                <a href="#">Home</a>
+                <a href="#">About</a>
+                <a href="#">Services</a>
+                <a href="#">Contact</a>
+            </div>
+            <div className="main-content">
+                <h1>d4t4</h1>
+            </div>
+            <div className="footer">
+                <p>Contact us: info@d4t4.xyz</p>
+            </div>
+        </div>
+    );
+}
+
+export default D4T4Page;
